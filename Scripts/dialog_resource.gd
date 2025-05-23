9328c7e0ec5739ff1099da2784d1e2a621718534
daistic <matotsuper@gmail.com>
2025-05-24 01:03:59 +0700

    resources & 3D testing

---
+++ Scripts/dialog_resource.gd
@@ -0,0 +1,5 @@
+extends Resource
+
+class_name DialogResource
+
+@export var lines: Array[String]
--- Scripts/test_dialog_node.gd
+++ Scripts/test_dialog_node.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@export var lines: Array[String] = []
+@export var lines: DialogResource
 
 func _input(event: InputEvent) -> void:
 	if (event.is_action_pressed("test_dialog")):
-		DialogManager.start_dialog(Vector2(100, 100), lines)
+		DialogManager.start_dialog(Vector2(500, 500), lines.lines)
