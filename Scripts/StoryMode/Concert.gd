cedc7bf7bd1103bcf0a36a4773449fd6179f8814
daistic <matotsuper@gmail.com>
2025-08-10 22:45:44 +0700

    final final final push

--- Scripts/StoryMode/Concert.gd
+++ Scripts/StoryMode/Concert.gd
@@ -1,8 +1,10 @@
 extends Control
 
+@export var next_episode: Global.EPISODE
+
 func _enter_tree() -> void:
 	SignalHub.concert_finished.connect(_go_to_epilogue)
 
 func _go_to_epilogue() -> void:
-	Global.change_playing_episode(Global.EPISODE_LIST[Global.EPISODE.EPILOGUE])
+	Global.change_playing_episode(Global.EPISODE_LIST[next_episode])
 	Global.go_to_story_mode()
