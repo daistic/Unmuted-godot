2ce2bf8171f8094dcce974540d0740c735e91fe1
daistic <matotsuper@gmail.com>
2025-08-10 17:10:58 +0700

    final push

--- Scripts/Main Menu/EpisodeSelectButton.gd
+++ Scripts/Main Menu/EpisodeSelectButton.gd
@@ -5,8 +5,7 @@ extends TextureButton
 @export var episode_data: Global.EPISODE
 
 func _ready() -> void:
-	pass
-	#label.text = Global.EPISODE_LIST[episode_data].episode_name
+	label.text = Global.EPISODE_LIST[episode_data].episode_name
 
 func _on_pressed() -> void:
 	Global.change_playing_episode(Global.EPISODE_LIST[episode_data])
